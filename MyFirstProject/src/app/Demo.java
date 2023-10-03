8cf341cbb269f6c649c8fbbd7c9d23cfab9b4fea
javeed512 <javeedmohammedjaveed@gmail.com>
2023-10-03 13:26:59 +0530

    Java Introduction Day1.1

---
+++ MyFirstProject/src/app/Demo.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("Thank you friends");
+		
+		
+	}
+
+}
